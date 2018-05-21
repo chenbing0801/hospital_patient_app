b6e5a9c2b61d6c837ff24efe235a9a5bfa820627
chenbing <chenbinging@outlook.com>
2018-05-21 23:02:35 +0800

    compile_top+content+bottom

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="width:100%; height:100%;">
+    <top></top>
+    <hr/>
+    <!--<divider>-</divider>-->
+    <div>content</div>
+    <bottom></bottom>
+  </div>
+</template>
+
+<script>
+import bottom from './views/Bottom'
+import top from './views/Top'
+import {Divider} from 'vux'
+
+export default {
+  components: {
+    bottom,
+    top,
+    Divider
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
+</style>
---
+++ src/components/views/Top.vue
@@ -0,0 +1,11 @@
+<template>
+  <span>标题</span>
+</template>
+
+<style>
+body{
+    text-align: center;
+    padding-top: 10px;
+    margin-bottom: 10px;
+}
+</style>
